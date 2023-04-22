5b988d01243917e542260480db31927939b65d56
PrayogaAjityaSetiawan <ajityaprayoga123@gmail.com>
2023-04-22 14:26:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+// const { default: plugin } = require('tailwindcss');
+const plugin = require('tailwindcss/plugin');
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FECD45',
+        'secondary': '#2568FB',
+        'all': '#000000'
+      },
+    fontFamily: {
+      'body': ['Inter']
+    },
+    },
+  },
+  plugins: [
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        '.horizontal-tb': {
+          'writing-mode': 'horizontal-tb',
+        },
+        '.vertical-rl': {
+          'writing-mode': 'vertical-rl',
+        },
+        '.vertical-lr': {
+          'writing-mode': 'vertical-lr',
+        },
+        '.mixed': {
+          'text-orientation': 'mixed'
+        },
+        '.upright': {
+          'text-orientation': 'upright'
+        }
+      })
+    })
+  ]
+}
+
